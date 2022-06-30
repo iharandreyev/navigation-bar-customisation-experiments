3b02640f7c486fdd1c365c9d9925fa25c5d40988
Andreyeu, Ihar <iandreyeu@scnsoft.com>
2022-06-30 15:56:51 +0400

    Extract `UINavigationBarAppearance` extension into its own file

--- UIKit/ui-kit/Common/Utilities/Styling/UIViewController+NavigationBarStyle.swift
+++ UIKit/ui-kit/Common/Utilities/Styling/UIViewController+NavigationBarStyle.swift
@@ -12,37 +12,3 @@ extension UIViewController {
         navigationItem.backButtonDisplayMode = .minimal
     }
 }
-
-extension UINavigationBarAppearance {
-    fileprivate convenience init(_ appearance: NavigationBarStyle.Appearance) {
-        self.init()
-
-        let backgroundColor = UIColor
-            .with(token: appearance.backgroundColor)
-        configureWithBackgroundColor(backgroundColor)
-
-        backgroundEffect = appearance.backgroundEffect.map(UIBlurEffect.with)
-        titleTextAttributes = StringAttributes(appearance.titleStyle)
-
-        let backIndicatorTintColor = UIColor
-            .with(token: appearance.titleStyle.color)
-        let backIndicatorImage = UIImage
-            .with(token: appearance.backIndicatorImage)
-            .tintedWith(color: backIndicatorTintColor)
-            .withRenderingMode(.alwaysOriginal)
-        setBackIndicatorImage(backIndicatorImage, transitionMaskImage: backIndicatorImage)
-
-        if !appearance.hasShadow {
-            shadowColor = .clear
-        }
-    }
-
-    private func configureWithBackgroundColor(_ color: UIColor) {
-        if color.isTransparent {
-            configureWithTransparentBackground()
-        } else {
-            configureWithDefaultBackground()
-            backgroundColor = color
-        }
-    }
-}
