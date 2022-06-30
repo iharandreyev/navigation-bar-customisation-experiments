391c6dbadc4eaf035ede039a9b4f5984b997ac8a
Andreyeu, Ihar <iandreyeu@scnsoft.com>
2022-06-30 16:45:04 +0400

    Apply whole-app navigation bar customisation with the new proxy

--- SwiftUI/swift-ui/App/MainApp.swift
+++ SwiftUI/swift-ui/App/MainApp.swift
@@ -7,6 +7,26 @@ struct MainApp: App {
             NavigationView {
                 FirstScreenFactory.createView()
             }
+            .onAppear {
+                UINavigationBar.configureCurrent { item in
+                    let appearance = UINavigationBarAppearance()
+
+                    let textColor = ColorToken.textTetriary
+                    appearance.backgroundColor = UIColor.with(token: .backgroundSecondary)
+                    appearance.largeTitleTextAttributes = StringAttributes(.init(font: .titleBigger, color: textColor))
+                    appearance.titleTextAttributes = StringAttributes(.init(font: .titleDefault, color: textColor))
+                    let backImage = UIImage.with(token: .backButton).tintedWith(color: UIColor.with(token: textColor)).withRenderingMode(.alwaysOriginal)
+                    appearance.setBackIndicatorImage(backImage, transitionMaskImage: backImage)
+                    appearance.backButtonAppearance.normal.titleTextAttributes = [
+                        .font: UIFont.systemFont(ofSize: 0),
+                        .foregroundColor: UIColor.clear
+                    ]
+
+                    item.scrollEdgeAppearance = appearance
+                    item.standardAppearance = appearance
+
+                }
+            }
         }
     }
 }
